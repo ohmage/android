32f3393246d7b765b1f69b89bdf21c0bff9279f3
Cameron Ketcham <cketcham@gmail.com>
2013-12-03 13:38:03 -0500

    added initial ohmage endpoint class

---
+++ ohmage/src/main/java/org/ohmage/app/Endpoints.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2013 ohmage
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ohmage.app;
+
+/**
+ * The ohmage API endpoints.
+ */
+public class Endpoints {
+
+    /**
+     * The UserAgent string supplied with all ohmage HTTP requests.
+     */
+    public static final String USER_AGENT = "ohmage Android Agent";
+
+    /**
+     * The protocol and hostname used to access the ohmage service.
+     */
+    public static final String API_HOST = "https://dev.ohmage.org";
+
+    /**
+     * The URL root used to access the ohmage API.
+     */
+    public static final String API_ROOT = API_HOST + "/ohmage";
+
+}
